241f6d0a54387c52e2cd4aea8f3d1b9b99c90670
Ivan-Yagilev <ivanrosk@mail.ru>
2023-11-18 23:57:25 +0300

    add sql

--- internal/entity/provider.go
+++ internal/entity/provider.go
@@ -1,6 +1,12 @@
 package entity
 
 type ProviderResponse struct {
-	Price float32 `json:"price" db:"price" binding:"required"`
-	Kpgz  string  `json:"kpgz" db:"kpgz" binding:"required"`
+	Supplier_inn string `json:"supplier_inn" db:"supplier_inn" binding:"-"`
+	Upd          int    `json:"upd" db:"upd" binding:"-"`
+	Contract     int    `json:"contract" db:"contract" binding:"-"`
+	Facap        int    `json:"facap" db:"facap" binding:"-"`
+	DoneContr    int    `json:"donecontr" db:"donecontr" binding:"-"`
+	Min          string `json:"min" db:"min" binding:"-"`
+	Max          string `json:"max" db:"max" binding:"-"`
+	BlockSum     string `json:"blocksum" db:"blocksum" binding:"-"`
 }
--- internal/repository/filter_postgres.go
+++ internal/repository/filter_postgres.go
@@ -17,10 +17,14 @@ func NewFilterPostgres(db *sqlx.DB) *FilterPostgres {
 }
 
 func (r *FilterPostgres) GetAllKpgz(kpgz string) ([]entity.ProviderResponse, error) {
-	var lists []entity.ProviderResponse
+	var (
+		lists []entity.ProviderResponse
+	)
 
-	query := fmt.Sprintf("SELECT price, kpgz FROM %s WHERE $1 LIKE kpgz;", ksTable)
+	query := fmt.Sprintf("SELECT * FROM ( SELECT DISTINCT supplier_inn, count(DISTINCT upd_id) as upd, count(DISTINCT contract_id) AS contract, count(CASE WHEN scheduled_delivery_date < actual_delivery_date AND scheduled_delivery_date != '1970-01-01 00:00:00.000000' THEN supplier_inn END) AS facap FROM contreactexec group by supplier_inn) as t1 LEFT JOIN (SELECT DISTINCT supplier_inn, count(*) AS doneContr, min(conclusion_date), max(conclusion_date) FROM contracts GROUP BY supplier_inn) as t2 on t1.supplier_inn = t2.supplier_inn LEFT JOIN (SELECT DISTINCT blocking.supplier_inn as blInn, sum(blocking.blocking_end_date-blocking.blocking_start_date) as blockSum FROM blocking JOIN contracts on blocking.supplier_inn = contracts.supplier_inn GROUP BY blocking.supplier_inn) as t3 ON t2.supplier_inn = t3.blInn LEFT JOIN (SELECT participant_inn, kpgz FROM ks) as bruh on t3.blInn = bruh.participant_inn WHERE kpgz LIKE '$1%'")
 	err := r.db.Select(&lists, query, kpgz)
+
+	fmt.Println(lists)
 
 	if err != nil {
 		return nil, nil
